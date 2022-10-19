8efcee5a7476a59fe835fef12cac2eecf05cde20
xkhaib00 <xkhaib00@vutbr.cz>
2022-10-19 12:48:38 +0200

    12

--- lab4-lcd/lib/lcd/lcd.h
+++ lab4-lcd/lib/lcd/lcd.h
@@ -339,7 +339,7 @@ extern void lcd_puts_p(const char *progmem_s);
  * @param    cmd instruction to send to LCD controller, see HD44780 data sheet
  * @return   none
  */
-extern void lcd_command(uint8_t cmd);
+extern void lcd_command(uint8_t cmd); //<--------------- Rika cipu co ma delat. Poslu na nejakou pozici
 
 
 /**
@@ -349,7 +349,7 @@ extern void lcd_command(uint8_t cmd);
  * @param    data byte to send to LCD controller, see HD44780 data sheet
  * @return   none
  */
-extern void lcd_data(uint8_t data);
+extern void lcd_data(uint8_t data); //<--------------- Poslu timto data
 
 
 /**
--- lab4-lcd/src/main.c
+++ lab4-lcd/src/main.c
@@ -43,17 +43,198 @@
  **********************************************************************/
 int main(void)
 {
+
+    /*
+    // Custom character definition using https://omerk.github.io/lcdchargen/
+    uint8_t customChar[8] = {
+        0b00111,
+        0b01110,
+        0b11100,
+        0b11000,
+        0b11100,
+        0b01110,
+        0b00111,
+        0b00011
+    };
+
+    uint8_t customChar2[8] = {
+        0b11001,
+        0b10011,
+        0b00110,
+        0b01100,
+        0b11001,
+        0b10011,
+        0b00110,
+        0b01100
+    };
+
+    uint8_t customChar3[8] = {
+        0b10101,
+        0b10101,
+        0b10001,
+        0b10101,
+        0b10101,
+        0b10001,
+        0b10101,
+        0b10101
+    };
+
+    uint8_t customChar4[8] = {
+        0b01010,
+        0b10101,
+        0b01010,
+        0b10101,
+        0b01010,
+        0b10101,
+        0b01010,
+        0b10101
+    };
+
+    uint8_t customChar5[8] = {
+        0b10010,
+        0b01001,
+        0b10010,
+        0b00100,
+        0b01001,
+        0b00100,
+        0b10010,
+        0b01001
+    };
+
+    uint8_t customChar6[8] = {
+        0b00100,
+        0b00100,
+        0b00100,
+        0b11111,
+        0b11111,
+        0b00100,
+        0b00100,
+        0b00100
+    };
+
+    uint8_t customChar7[8] = {
+        0b00110,
+        0b00110,
+        0b11001,
+        0b11001,
+        0b00110,
+        0b00110,
+        0b11001,
+        0b11001
+    };
+
+
+
+    // Initialize LCD display
+    lcd_init(LCD_DISP_ON);
+
+    // Set pointer to beginning of CGRAM memory
+    lcd_command(1 << LCD_CGRAM);
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        // Store all new chars to memory line by line
+        lcd_data(customChar[i]);
+    }
+
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        // Store all new chars to memory line by line
+        lcd_data(customChar2[i]);
+    }
+
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        // Store all new chars to memory line by line
+        lcd_data(customChar3[i]);
+    }
+
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        // Store all new chars to memory line by line
+        lcd_data(customChar4[i]);
+    }
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        // Store all new chars to memory line by line
+        lcd_data(customChar5[i]);
+    }
+
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        // Store all new chars to memory line by line
+        lcd_data(customChar6[i]);
+    }
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        // Store all new chars to memory line by line
+        lcd_data(customChar7[i]);
+    }
+
+    // Set DDRAM address to display characters
+    lcd_command(1 << LCD_DDRAM);
+ 
+    // Display first custom character at address 0
+    lcd_putc(0x00);
+
+    lcd_gotoxy(2, 0);
+    lcd_putc(0x01);
+
+    lcd_gotoxy(4, 0);
+    lcd_putc(0x02);
+
+    lcd_gotoxy(6, 0);
+    lcd_putc(0x03);
+
+    lcd_gotoxy(8, 0);
+    lcd_putc(0x04);
+
+    lcd_gotoxy(10, 0);
+    lcd_putc(0x05);
+
+    lcd_gotoxy(12, 0);
+    lcd_putc(0x06);
+
+    */
+
+    
+
+
+
+    //__________________________________________________________________
+    
     // Initialize display
     lcd_init(LCD_DISP_ON_CURSOR_BLINK);
 
     // Put string(s) on LCD screen
-    lcd_gotoxy(6, 1);
-    lcd_puts("LCD Test");
-    lcd_putc('!');
+    /*
+    lcd_gotoxy(6, 0);
+    lcd_puts("LCD Test"); // Zapis Retezce
+    lcd_putc('!');        // Zapis symbolu
+    */ 
+    
+
+    
+
 
     // Configuration of 8-bit Timer/Counter2 for Stopwatch update
     // Set the overflow prescaler to 16 ms and enable interrupt
+    TIM2_overflow_16ms();
+    TIM2_overflow_interrupt_enable();
 
+    uint8_t customChar[] = {
+    // addr 0: .....
+    0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000,
+    // addr 1: |....
+    0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000,
+    // addr 2: ||...
+    0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000,
+    // addr 3: |||..
+    0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100,
+    // addr 4: ||||.
+    0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110,
+    // addr 4: |||||
+    0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111,
+    };
 
     // Enables interrupts by setting the global interrupt mask
     sei();
@@ -80,6 +261,10 @@ ISR(TIMER2_OVF_vect)
 {
     static uint8_t no_of_overflows = 0;
     static uint8_t tenths = 0;  // Tenths of a second
+    static uint8_t seconds = 0;
+    static uint8_t minutes = 0;
+    static uint8_t symbol = 0;
+    static uint8_t position = 0;
     char string[2];             // String for converted numbers by itoa()
 
     no_of_overflows++;
@@ -89,12 +274,83 @@ ISR(TIMER2_OVF_vect)
         no_of_overflows = 0;
 
         // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
+        
+        tenths++;
+
+        if (tenths > 9)  
+        {
+          tenths =0;
+          seconds++;
+          if (seconds > 59)
+          {
+            seconds =0;
+            minutes++;
+          }
+        }           
 
 
         itoa(tenths, string, 10);  // Convert decimal value to string
         // Display "00:00.tenths"
         lcd_gotoxy(7, 0);
         lcd_puts(string);
+      
+        lcd_gotoxy(6, 0);
+        lcd_putc('.'); 
+
+        itoa(seconds, string, 10);
+        if (seconds <=9 )
+        {
+        lcd_gotoxy(5, 0);
+        lcd_puts(string);
+        lcd_gotoxy(4, 0);
+        lcd_putc('0');
+        }
+        else
+        {
+        lcd_gotoxy(4, 0);
+        lcd_puts(string);
+        }
+
+        lcd_gotoxy(3, 0);
+        lcd_putc(':'); 
+
+        itoa(minutes, string, 10);
+        lcd_gotoxy(2, 0);
+        lcd_puts(string);
+        lcd_gotoxy(1, 0);
+        lcd_putc('0');
+
+        /*
+        lcd_gotoxy(11, 0);
+        lcd_putc('a');        
+        lcd_gotoxy(1, 1);
+        lcd_putc('b');        
+        lcd_gotoxy(11, 1);
+        lcd_putc('c');
+        */
+       
+
+       //int n = 0;
+
+       if (seconds == 1) 
+        {
+          lcd_gotoxy(1, 1);
+          lcd_putc(0xff);
+        }
+        if (seconds == 2) 
+        {
+          lcd_gotoxy(2, 1);
+          lcd_putc(0xff);
+        }
+
+        //lcd_gotoxy(1 + position, 1);
+        //lcd_putc(symbol);
+
+
+
+        lcd_gotoxy(16, 1);  
+
+
     }
     // Else do nothing and exit the ISR
 }
