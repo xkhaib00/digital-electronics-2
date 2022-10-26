46478e89bc5f28487685ae11ccce77d4e522cbcf
xkhaib00 <xkhaib00@vutbr.cz>
2022-10-26 11:06:01 +0200

    dss

---
+++ lab5-adc/include/timer.h
@@ -0,0 +1,82 @@
+#ifndef TIMER_H
+# define TIMER_H
+
+/***********************************************************************
+ * 
+ * Timer library for AVR-GCC.
+ * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+ *
+ * Copyright (c) 2019 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ *
+ **********************************************************************/
+
+/**
+ * @file 
+ * @defgroup fryza_timer Timer Library <timer.h>
+ * @code #include "timer.h" @endcode
+ *
+ * @brief Timer library for AVR-GCC.
+ *
+ * The library contains macros for controlling the timer modules.
+ *
+ * @note Based on Microchip Atmel ATmega328P manual and no source file
+ *       is needed for the library.
+ * @author Tomas Fryza, Dept. of Radio Electronics, Brno University 
+ *         of Technology, Czechia
+ * @copyright (c) 2019 Tomas Fryza, This work is licensed under 
+ *                the terms of the MIT license
+ * @{
+ */
+
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>
+
+
+/* Defines -----------------------------------------------------------*/
+/**
+ * @name  Definitions for 8-bit Timer/Counter2
+ * @note  t_OVF = 1/F_CPU * prescaler * 2^n where n = 8, F_CPU = 16 MHz
+ */
+/** @brief Stop timer,         prescaler 000 --> STOP */
+#define TIM2_stop()            TCCR2B &= ~((1<<CS22) | (1<<CS21) | (1<<CS20));
+/** @brief Set overflow 16us,  prescaler 001 --> 1    */
+#define TIM2_overflow_16us()   TCCR2B &= ~((1<<CS22) | (1<<CS21)); TCCR2B |= (1<<CS20);
+/** @brief Set overflow 128us, prescaler 010 --> 8    */
+#define TIM2_overflow_128us()  TCCR2B &= ~((1<<CS22) | (1<<CS20)); TCCR2B |= (1<<CS21);
+/** @brief Set overflow 512us, prescaler 011 --> 32   */
+#define TIM2_overflow_512us()  TCCR2B &= ~(1<<CS22); TCCR2B |= (1<<CS21) | (1<<CS20);
+/** @brief Set overflow 1ms,   prescaler 100 --> 64   */
+#define TIM2_overflow_1ms()    TCCR2B &= ~((1<<CS21) | (1<<CS20)); TCCR2B |= (1<<CS22);
+/** @brief Set overflow 2ms,   prescaler 101 --> 128  */
+#define TIM2_overflow_2ms()    TCCR2B &= ~(1<<CS21); TCCR2B |= (1<<CS22) | (1<<CS20);
+/** @brief Set overflow 4ms,   prescaler 110 --> 256  */
+#define TIM2_overflow_4ms()    TCCR2B &= ~((1<<CS20); TCCR2B |= ((1<<CS22)| (1<<CS21));;
+/** @brief Set overflow 16,s,  prescaler 111 --> 1024 */
+#define TIM2_overflow_16ms()   TCCR2B |= (1<<CS22) | (1<<CS21) | (1<<CS20);
+
+/** @brief Enable overflow interrupt,  1 --> enable */
+#define TIM2_overflow_interrupt_enable()  TIMSK2 |= (1<<TOIE2);
+/** @brief Disable overflow interrupt, 0 --> disable */
+#define TIM2_overflow_interrupt_disable() TIMSK2 &= ~(1<<TOIE2);
+
+
+/**
+ * @name  Definitions for 8-bit Timer/Counter0
+ * @note  t_OVF = 1/F_CPU * prescaler * 2^n where n = 8, F_CPU = 16 MHz
+ */
+// WRITE YOUR CODE HERE
+
+
+/**
+ * @name  Definitions for 8-bit Timer/Counter2
+ * @note  t_OVF = 1/F_CPU * prescaler * 2^n where n = 8, F_CPU = 16 MHz
+ */
+// WRITE YOUR CODE HERE
+
+
+/** @} */
+
+#endif
