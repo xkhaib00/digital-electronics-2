d9c91964ea22a8d23204f1dd3b2f0f263d5bee96
xkhaib00 <xkhaib00@vutbr.cz>
2022-11-24 14:15:23 +0100

    123

--- TestProjects/src/main.c
+++ TestProjects/src/main.c
@@ -2,13 +2,15 @@
 #include <util/delay.h>
 #include <Arduino.h>
 
-#define LED PD2 
+//#define LED PD2 
 //#define PD2 D2
 
-void setup() {
-  
-}
+#define pinX    A2  // ось X джойстика
+#define pinY    A1  // ось Y джойстика
+#define swPin    2  // кнопка джойстика
+#define ledPin  13  // светодиод на Pin 13
 
+/*
 int main(void) {
 
   uint8_t led_value = LOW;
@@ -27,4 +29,28 @@ int main(void) {
 
   return 0;
 
+}
+*/
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(pinX, INPUT);
+  pinMode(pinY, INPUT);
+  
+  pinMode(swPin, INPUT);
+  digitalWrite(swPin, HIGH);
+}
+ 
+void loop() {
+  boolean ledState = digitalRead(swPin); // считываем состояние кнопки
+  digitalWrite(ledPin, ledState);        // вкл./выкл. светодиод
+
+  int X = analogRead(pinX);              // считываем значение оси Х
+  int Y = analogRead(pinY);              // считываем значение оси Y
+
+  Serial.print(X);                       // выводим в Serial Monitor
+  Serial.print("\t");                    // табуляция
+  Serial.println(Y);
 }
