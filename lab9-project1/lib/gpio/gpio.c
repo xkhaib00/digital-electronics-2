4283dd99c36124245444af604d32a44735e8af89
xkhaib00 <xkhaib00@vutbr.cz>
2022-11-30 12:41:38 +0100

    1212

--- lab9-project1/lib/gpio/gpio.c
+++ lab9-project1/lib/gpio/gpio.c
@@ -94,6 +94,12 @@ uint8_t GPIO_read(volatile uint8_t *reg, uint8_t pin)
 /**********************************************************************
  * Function: GPIO_mode_input_nopull()
  **********************************************************************/
+void GPIO_mode_input_nopullup(volatile uint8_t *reg, uint8_t pin)
+{
+    *reg = *reg & ~(1<<pin);  // Data Direction Register
+    reg++;                    // Change pointer to Data Register
+    *reg = *reg & ~(1<<pin);   // Data Register
+}
 
 
 /**********************************************************************
